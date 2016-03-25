2629116351be0e4af3569ab736f20ab81eb22d13
Nan Ma <nma@yammer-inc.com>
2016-03-24 18:13:20 -0700

    Debugging branch for Nan

--- background.js
+++ background.js
@@ -8,53 +8,62 @@ var selectedWord = null;
 var selectedId = null;
 var logmsg = 'original';
 
-function updateWord(tabId) {
-  chrome.tabs.sendRequest(tabId, {}, function(word) {
-    words[tabId] = word;
-    if (!word) {
-      chrome.pageAction.show(tabId);
-      if (selectedId == tabId) {
-        updateSelected(tabId);
-      }
-    } else {
-      chrome.pageAction.show(tabId);
-      if (selectedId == tabId) {
-        updateSelected(tabId);
-      }
-    }
-  });
-}
+// function updateWord(tabId) {
+//   chrome.tabs.sendRequest(tabId, {}, function(word) {
+//     words[tabId] = word;
+//     if (!word) {
+//       chrome.pageAction.show(tabId);
+//       if (selectedId == tabId) {
+//         updateSelected(tabId);
+//       }
+//     } else {
+//       chrome.pageAction.show(tabId);
+//       if (selectedId == tabId) {
+//         updateSelected(tabId);
+//       }
+//     }
+//   });
+// }
 
-function updateSelected(tabId) {
-  selectedWord = words[tabId];
-  if (selectedWord)
-    chrome.pageAction.setTitle({tabId:tabId, title:selectedWord});
-}
+// function updateSelected(tabId) {
+//   selectedWord = words[tabId];
+//   if (selectedWord)
+//     chrome.pageAction.setTitle({tabId:tabId, title:selectedWord});
+// }
 
-chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
-  if (change.status == "complete") {
-    updateWord(tabId);
-    logmsg = "hello hello " + tabId;
+// chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
+//   if (change.status == "complete") {
+//     updateWord(tabId);
+//     logmsg = "hello hello " + tabId;
+//   }
+// });
+
+// chrome.tabs.onSelectionChanged.addListener(function(tabId, info) {
+//   selectedId = tabId;
+//   updateSelected(tabId);
+// });
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if ((message.type) == "sendText") {
+    alert("got text ");
+    // sendResponse(findWord());
+
   }
 });
 
-chrome.tabs.onSelectionChanged.addListener(function(tabId, info) {
-  selectedId = tabId;
-  updateSelected(tabId);
-});
-
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response){
-    var word = response.data;
-    words[tab.id] = word;
-    if (!word) {
-      if (selectedId == tab.id) {
-        updateSelected(tab.id);
-      };
-    };
-  });
-});
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   alert("clicked")
+//   chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response){
+//     alert("got selection in background " + response);
+//     // var word = response.data;
+//     // words[tab.id] = word;
+//     // if (!word) {
+//     //   if (selectedId == tab.id) {
+//     //     updateSelected(tab.id);
+//     //   };
+//     // };
+//   });
+// });
 
 // Ensure the current selected tab is set up.
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
--- translator_content_script.js
+++ translator_content_script.js
@@ -1,20 +1,22 @@
+alert("here");
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 // The background page is asking us to find a word on the page.
-if (window == top) {
-  chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    sendResponse(findWord());
-  });
-};
+// if (window == top) {
+//   chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+//     sendResponse(findWord());
+//   });
+// };
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    if (request.method == "getSelection")
-      sendResponse({data: window.getSelection().toString()});
-    else
-      sendResponse({}); // snub them.
-});
+// chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+//     alert("got request");
+//     if (request.method == "getSelection")
+//       sendResponse({data: window.getSelection().toString()});
+//     else
+//       sendResponse({}); // snub them.
+// });
 
 // Search the text for selected word
 // Return null if none is found.
@@ -23,6 +25,15 @@ var findWord = function() {
   var word = window.getSelection().toString();
 };
 
-window.onselect.addListener()
+var sendSelectedText = function(){
+  alert('send');
+  chrome.runtime.sendMessage({"type": "sendText", "text": "this is a test"});
+};
 
-
+// window.onselect.addListener()
+var bodies = document.getElementsByTagName("body");
+console.log(bodies.length);
+if (bodies) {
+  // bodies[0].onmouseup = sendSelectedText;
+  bodies[0].onmouseup = function(){alert("mouse up");};
+}
