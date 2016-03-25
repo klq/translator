e28d131c165477ab48f92ef3136d41461d9a9183
Lingqiang Kong <konglq@gmail.com>
2016-03-25 16:30:01 -0700

    add spinner icon when loading, cosmetic changes too

--- popup.js
+++ popup.js
@@ -1,23 +1,37 @@
 function displayTranslation(translatedText) {
-  $('#giphy').attr('src', translatedText);
+  $('#translation').text(translatedText);
+};
+
+function displayGiphy(imgUrl) {
+  $('#giphy').attr('src', imgUrl);
 };
 
 function getTranslation(word, displayCallback) {
+  var translatedText = "gato gato gato!!";
+  if (translatedText) {
+    displayCallback(translatedText);
+  } else {
+    displayCallback("Sorry! Word: \'" + word + "\' not found. ")
+  };
+};
+
+function getGiphy(word, displayCallback) {
   $.get("https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + word, function(response) {
     var data = response.data;
     if (data) {
-      var firstImageUrl = data[0].images.original.url;
+      var firstImageUrl = data[0].images.fixed_height.url;
       displayCallback(firstImageUrl);
-    }
+    };
   });
-}
+};
 
 function translate() {
   var word = chrome.extension.getBackgroundPage().selectedWord;
   if (word) {
     getTranslation(word, displayTranslation);
-  }
-}
+    getGiphy(word, displayGiphy);
+  };
+};
 
 window.onload = translate;
 window.onclick = window.close;
