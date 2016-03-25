f3f2ee132e23dccaa2d6bc0d5136451703a9deed
nanma80 <mananself@gmail.com>
2016-03-24 22:33:28 -0700

    Giphy works but no error handling

--- popup.js
+++ popup.js
@@ -1,18 +1,21 @@
 function displayTranslation(translatedText) {
-  var translation = $('#translation');
-  translation.text(translatedText);
+  $('#giphy').attr('src', translatedText);
 };
 
-function getTranslation(word) {
-  // TODO: call translation service
-  return word;
+function getTranslation(word, displayCallback) {
+  $.get("https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + word, function(response) {
+    var data = response.data;
+    if (data) {
+      var firstImageUrl = data[0].images.original.url;
+      displayCallback(firstImageUrl);
+    }
+  });
 }
 
 function translate() {
   var word = chrome.extension.getBackgroundPage().selectedWord;
   if (word) {
-    translatedText = getTranslation(word);
-    displayTranslation(translatedText);
+    getTranslation(word, displayTranslation);
   }
 }
 
